2eb871fc2de940aa73b9d63d53d93ea7f088b173
Daniel Sousa <daniel.s@crowdbotics.com>
2024-01-18 18:42:42 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "icy-boat-3",
+  url: "https://icy-boat-3.botics.co",
+  api: "https://icy-boat-3.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
